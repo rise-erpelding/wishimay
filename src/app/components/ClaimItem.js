33585b7dd9894a870ab5e4ef641cbc68d6071881
Rise Erpelding <rise.erpelding@gmail.com>
2024-11-23 15:35:56 -0600

    feat: conditionally render claim item

--- src/app/components/ClaimItem.js
+++ src/app/components/ClaimItem.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { patchEntry } from '../../lib/contentful';
 import ClaimItemForm from './ClaimItemForm';
 import './css/ClaimItem.css';
@@ -7,39 +7,66 @@ import './css/ClaimItem.css';
 const ClaimItem = ({ currentItem }) => {
   const [showForm, setShowForm] = useState(false);
   const [showClaimed, setShowClaimed] = useState(false);
-  
+  const existingClaimedStatus = currentItem.fields.isClaimed;
   const itemId = currentItem.sys.id;
+
+  useEffect(() => {
+    setShowClaimed(existingClaimedStatus);
+  }, [existingClaimedStatus]);
 
   const handleClaim = () => {
     setShowForm(!showForm);
   }
 
-  const handleUnclaim = (e) => {
+  const handleUnclaim = async (e) => {
     e.preventDefault();
-    patchEntry(itemId, {isClaimed: false, claimedBy: ""})
-    setShowClaimed(false);
-  }
+    try {
+      await patchEntry(itemId, { isClaimed: false, claimedBy: "" });
+      setShowClaimed(false);
+    } catch (error) {
+      console.error('Failed to unclaim the item:', error);
+    }
+  };
 
   return (
-    <div>
-      <h5>Planning to buy this? Claim it!</h5>
-      <button
-        className="claim-item-button border-solid border-2"
-        onClick={handleClaim}
-      >
-        Claim
-      </button>
-      <ClaimItemForm showForm={showForm} itemId={itemId} handleShowClaimed={setShowClaimed} />
-      <div className={showClaimed ? "" : "hidden"}>
-        <p>Claimed!</p>
-        <button
-          className="claim-item-button border-solid border-2"
-          onClick={handleUnclaim}
-        >
-          Undo?
-        </button>
-      </div>
-    </div>
+    <>
+      {existingClaimedStatus ? (
+        <div>
+          <h5>Already claimed</h5>
+        </div>
+      ) : (
+        <>
+          {showClaimed ? (
+            <div>
+              <p>Claimed!</p>
+              <button
+                className="claim-item-button border-solid border-2"
+                onClick={handleUnclaim}
+              >
+                Undo?
+              </button>
+            </div>
+          ) : (
+            <div>
+              <h5>Planning to buy this? Claim it!</h5>
+              <button
+                className="claim-item-button border-solid border-2"
+                onClick={handleClaim}
+              >
+                Claim
+              </button>
+            </div>
+          )}
+        </>
+      )}
+      {showForm && (
+        <ClaimItemForm
+          showForm={showForm}
+          itemId={itemId}
+          handleShowClaimed={setShowClaimed}
+        />
+      )}
+    </>
   );
 };
 
