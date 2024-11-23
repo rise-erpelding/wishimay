027a232e45017ec271128afbb891a48b3ed8960e
Rise Erpelding <rise.erpelding@gmail.com>
2024-11-23 14:25:37 -0600

    feat: style claim item form

---
+++ src/app/components/Icons.js
@@ -0,0 +1,11 @@
+export const CloseButton = ({ size }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 100 100"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <line x1="10" y1="10" x2="90" y2="90" stroke="black" strokeWidth="10" />
+    <line x1="90" y1="10" x2="10" y2="90" stroke="black" strokeWidth="10" />
+  </svg>
+);
