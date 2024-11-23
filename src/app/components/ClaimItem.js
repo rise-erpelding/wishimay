8ced75d47ed2d0341c61affc806e8906b4a2e87e
Rise Erpelding <rise.erpelding@gmail.com>
2024-11-23 13:24:15 -0600

    feat: move form to own component

--- src/app/components/ClaimItem.js
+++ src/app/components/ClaimItem.js
@@ -1,33 +1,17 @@
 "use client";
 import { useState } from 'react';
 import { patchEntry } from '../../lib/contentful';
+import ClaimItemForm from './ClaimItemForm';
 import './css/ClaimItem.css';
 
 const ClaimItem = ({ currentItem }) => {
   const [showForm, setShowForm] = useState(false);
   const [showClaimed, setShowClaimed] = useState(false);
-  const [emailAddress, setEmailAddress] = useState("");
-
+  
   const itemId = currentItem.sys.id;
-
-  const handleInputChange = (e) => {
-    setEmailAddress(e.target.value);
-  }
 
   const handleClaim = () => {
     setShowForm(!showForm);
-  }
-
-  const handleClaimWithoutEmail = (e) => {
-    e.preventDefault();
-    setShowClaimed(true);
-    patchEntry(itemId, {isClaimed: true});
-  }
-
-  const handleClaimWithEmail = (e) => {
-    e.preventDefault();
-    patchEntry(itemId, {isClaimed: true, claimedBy: emailAddress})
-    setShowClaimed(true);
   }
 
   const handleUnclaim = (e) => {
@@ -45,39 +29,16 @@ const ClaimItem = ({ currentItem }) => {
       >
         Claim
       </button>
-      <form className={showForm ? "claim-item-form" : "claim-item-form hidden"}>
-        <label htmlFor="email">Enter email address:</label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          value={emailAddress}
-          onChange={handleInputChange}
-        ></input>
-        <div className="button-group">
-          <button
-            onClick={handleClaimWithoutEmail}
-            className="claim-item-button border-solid border-2"
-          >
-            Claim without email
-          </button>
-          <button
-            onClick={handleClaimWithEmail}
-            className="claim-item-button border-solid border-2"
-          >
-            Claim with email
-          </button>
-        </div>
-        <div className={showClaimed ? "" : "hidden"}>
-          <p>Claimed!</p>
-          <button
-            className="claim-item-button border-solid border-2"
-            onClick={handleUnclaim}
-          >
-            Undo?
-          </button>
-        </div>
-      </form>
+      <ClaimItemForm showForm={showForm} itemId={itemId} handleShowClaimed={setShowClaimed} />
+      <div className={showClaimed ? "" : "hidden"}>
+        <p>Claimed!</p>
+        <button
+          className="claim-item-button border-solid border-2"
+          onClick={handleUnclaim}
+        >
+          Undo?
+        </button>
+      </div>
     </div>
   );
 };
