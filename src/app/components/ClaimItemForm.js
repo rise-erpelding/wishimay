b6bbd9765a0e2413b7167de186482b4b82d6af5e
Rise Erpelding <rise.erpelding@gmail.com>
2024-11-23 14:25:37 -0600

    feat: style claim item form

--- src/app/components/ClaimItemForm.js
+++ src/app/components/ClaimItemForm.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from 'react';
 import { patchEntry } from '../../lib/contentful';
-import './css/ClaimItem.css';
+import { CloseButton } from './Icons';
 
 const ClaimItemForm = ({ showForm, itemId, handleShowClaimed }) => {
   const [emailAddress, setEmailAddress] = useState("");
@@ -23,31 +23,37 @@ const ClaimItemForm = ({ showForm, itemId, handleShowClaimed }) => {
   }
 
   return (
-    <form className={showForm ? "claim-item-form" : "claim-item-form hidden"}>
-      <label htmlFor="email">Enter email address:</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        value={emailAddress}
-        onChange={handleInputChange}
-      ></input>
-      <div className="button-group">
-        <button
-          onClick={handleClaimWithoutEmail}
-          className="claim-item-button border-solid border-2"
-        >
-          Claim without email
-        </button>
-        <button
-          onClick={handleClaimWithEmail}
-          className="claim-item-button border-solid border-2"
-        >
-          Claim with email
-        </button>
+    <div className={showForm ? "bg-white p-6" : "bg-white p-6 hidden"}>
+      <div className="flex flex-row-reverse">
+        <CloseButton size={16} />
       </div>
-
-    </form>
+      <form className={showForm ? "" : "hidden"}>
+        <label htmlFor="email" className="block mb-1">Email</label>
+        <input
+          className="block border border-neutral-500 w-full h-10 rounded-lg"
+          id="email"
+          name="email"
+          type="email"
+          value={emailAddress}
+          onChange={handleInputChange}
+        ></input>
+        <p className="text-neutral-700 mb-4 mt-1">Only visible to list owner</p>
+        <div className="button-group flex gap-3">
+          <button
+            onClick={handleClaimWithoutEmail}
+            className="border border-black border-solid rounded-lg p-2"
+          >
+            Claim without email
+          </button>
+          <button
+            onClick={handleClaimWithEmail}
+            className="border-transparent rounded-lg bg-blue-600 text-white p-2"
+          >
+            Claim item
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
