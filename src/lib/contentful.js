0680f18bbca5bd27c4c3516dac9e8427c6b66f4d
Rise Erpelding <rise.erpelding@gmail.com>
2024-11-24 21:40:19 -0600

    feat: add revalidate

--- src/lib/contentful.js
+++ src/lib/contentful.js
@@ -4,21 +4,30 @@ import { createClient as createManagementClient } from 'contentful-management';
 const client = createDeliveryClient({
     space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
     accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+    environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
 });
 
 const managementClient = createManagementClient({
     accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_ACCESS_TOKEN,
 });
 
-export async function fetchEntries() {
-    const entries = await client.getEntries();
-    return entries.items;
+export async function fetchEntries(content_type, options = {}) {
+  try {
+    const entries = await client.getEntries({
+      content_type,
+      ...options
+    })
+    return entries.items
+  } catch (error) {
+    console.error('Error fetching entries:', error)
+    return []
+  }
 }
 
 export async function patchEntry(entryId, updatedFields) {
     try {
         const space = await managementClient.getSpace(process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID);
-        const environment = await space.getEnvironment('master');
+        const environment = await space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT || 'master');
 
         const entry = await environment.getEntry(entryId);
 
