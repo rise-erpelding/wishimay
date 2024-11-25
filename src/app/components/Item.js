200e45375fe7eeef078773c6bf864340ae348f99
Rise Erpelding <rise.erpelding@gmail.com>
2024-11-24 20:18:08 -0600

    fix: adjust breakpoints

--- src/app/components/Item.js
+++ src/app/components/Item.js
@@ -5,18 +5,18 @@ import './css/Item.css';
 const Item = ({ currentItem }) => {
   const isClaimed = currentItem.fields.isClaimed;
   return (
-    <li className="item p-6 mb-2 rounded-lg bg-white shadow">
+    <li className="item p-6 mb-2 rounded-lg bg-white shadow max-w-4xl">
       <div className="item-title-section">
         {currentItem.fields.url ? (
           <a href={currentItem.fields.url}>
-            <h4 className="font-semibold mb-1 text-2xl sm:text-xl">{currentItem.fields.title}</h4>
+            <h4 className="font-semibold mb-1 text-2xl md:text-xl">{currentItem.fields.title}</h4>
           </a>
         ) : (
-          <h4 className={isClaimed ? "font-semibold mb-1 text-2xl sm:text-xl line-through" : "font-semibold mb-1 text-2xl sm:text-xl"}>{currentItem.fields.title}</h4>
+          <h4 className={isClaimed ? "font-semibold mb-1 text-2xl md:text-xl line-through" : "font-semibold mb-1 text-2xl md:text-xl"}>{currentItem.fields.title}</h4>
         )}
       </div>
       <div className="item-description-section">
-        {currentItem.fields.description && <p className={isClaimed ? "text-xl sm:text-base line-through" : "text-xl sm:text-base"}>{currentItem.fields.description}</p>}
+        {currentItem.fields.description && <p className={isClaimed ? "text-xl md:text-base line-through" : "text-xl md:text-base"}>{currentItem.fields.description}</p>}
       </div>
       <div className="item-image-section flex justify-center">
         <img
