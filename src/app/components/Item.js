172c051de4340ba782dc9f498247ea4e770cebd9
Rise Erpelding <rise.erpelding@gmail.com>
2024-11-27 08:57:51 -0600

    fix: improve link behavior
    - adds underline to title text if there is a link
    - adds anchor to image if there is a link
    
    Closes #9

--- src/app/components/Item.js
+++ src/app/components/Item.js
@@ -8,7 +8,7 @@ const Item = ({ currentItem }) => {
     <li className="item p-6 mb-2 rounded-lg bg-white shadow max-w-4xl">
       <div className="item-title-section">
         {currentItem.fields.url ? (
-          <a href={currentItem.fields.url}>
+          <a href={currentItem.fields.url} className="underline">
             <h4 className="font-semibold mb-1 text-2xl md:text-xl">{currentItem.fields.title}</h4>
           </a>
         ) : (
@@ -19,11 +19,21 @@ const Item = ({ currentItem }) => {
         {currentItem.fields.description && <p className={isClaimed ? "text-xl md:text-base line-through" : "text-xl md:text-base"}>{currentItem.fields.description}</p>}
       </div>
       <div className="item-image-section flex justify-center">
-        <img
-          alt={currentItem.fields.title}
-          src={currentItem.fields.imageUrl || '/shopping-bags.jpg'}
-          className="item-image"
-        />
+        {currentItem.fields.url ? (
+          <a href={currentItem.fields.url}>
+            <img
+              alt={currentItem.fields.title}
+              src={currentItem.fields.imageUrl || '/shopping-bags.jpg'}
+              className="item-image"
+            />
+          </a>
+        ) : (
+          <img
+            alt={currentItem.fields.title}
+            src={currentItem.fields.imageUrl || '/shopping-bags.jpg'}
+            className="item-image"
+          />
+          )}
       </div>
       <div className="item-claim-item-section">
         <ClaimItem currentItem={currentItem} />
