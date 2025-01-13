9e4078d79088ca6db4dd47cd587bcae18e7aad3a
Rise Erpelding <rise@heysparkbox.com>
2025-01-12 21:10:49 -0600

    feat: accordion type buttons to collapse content

--- src/app/components/WishList.js
+++ src/app/components/WishList.js
@@ -1,6 +1,12 @@
+"use client";
+import { useState } from 'react';
 import Category from './Category';
+import { ChevronDown, ChevronUp } from './Icons';
 
 const WishList = ({ currentWishList, categories, items }) => {
+  const [isShowingWishList, setIsShowingWishList] = useState("true");
+  const toggleShowWishList = () => setIsShowingWishList(!isShowingWishList);
+
   const currentWishListId = currentWishList.sys.id;
   const wishListCategories = categories.filter(category => 
     category.fields.wishList?.some(wishListItem => wishListItem.sys.id === currentWishListId)
@@ -8,8 +14,13 @@ const WishList = ({ currentWishList, categories, items }) => {
 
   return (
     <section className="wish-list mb-8 p-4 rounded">
-      <h2 className="text-3xl font-black mb-2">{currentWishList.fields.title}</h2>
-      <div>
+      <h2 className="text-3xl font-black mb-2">
+        <button onClick={toggleShowWishList}>
+          {currentWishList.fields.title}
+          {isShowingWishList ? <ChevronUp size={16} /> : <ChevronDown size={16} />}
+        </button>
+      </h2>
+      <div className={isShowingWishList ? "is-open" : "is-closed"}>
         {wishListCategories.map(wishListCategory => (
           <Category key={wishListCategory.sys.id} currentWishList={currentWishList} currentCategory={wishListCategory} items={items} />
         ))}
