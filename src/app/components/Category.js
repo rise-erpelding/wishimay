55a52c32c675725756f07c3923a0d023d507e7d3
Rise Erpelding <rise@heysparkbox.com>
2025-01-12 21:10:49 -0600

    feat: accordion type buttons to collapse content

--- src/app/components/Category.js
+++ src/app/components/Category.js
@@ -1,6 +1,12 @@
+"use client";
+import { useState } from 'react';
 import Item from './Item';
+import { ChevronDown, ChevronUp } from './Icons';
 
 const Category = ({ currentWishList, currentCategory, items }) => {
+  const [isShowingCategory, setIsShowingCategory] = useState("true");
+  const toggleShowCategory = () => setIsShowingCategory(!isShowingCategory);
+
   const currentWishListId = currentWishList.sys.id;
   const currentCategoryId = currentCategory.sys.id;
   const categoryItems = items.filter(item => (
@@ -19,8 +25,13 @@ const Category = ({ currentWishList, currentCategory, items }) => {
 
   return (
     <section className="category mb-2 p-3 rounded">
-      <h3 className="text-lg font-extrabold uppercase mb-2">{currentCategory.fields.title}</h3>
-      <ul>
+      <h3 className="text-lg font-extrabold uppercase mb-2">
+        <button onClick={toggleShowCategory}>
+          {currentCategory.fields.title}
+          {isShowingCategory ? <ChevronUp size={12} /> : <ChevronDown size={12} />}
+        </button>
+      </h3>
+      <ul className={isShowingCategory ? "is-open" : "is-closed"}>
         {sortedCategoryItems.map(categoryItem => (
           <Item key={categoryItem.sys.id} currentItem={categoryItem} />
         ))}
--- src/app/components/Icons.js
+++ src/app/components/Icons.js
@@ -9,3 +9,33 @@ export const CloseButton = ({ size }) => (
     <line x1="90" y1="10" x2="10" y2="90" stroke="black" strokeWidth="10" />
   </svg>
 );
+
+export const ChevronUp = ({ size }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 10 10"
+    xmlns="http://www.w3.org/2000/svg"
+    className="inline-block mx-1"
+  >
+    <path
+      d="M3 9.95a.875.875 0 0 1-.615-1.498L5.88 5 2.385 1.547A.875.875 0 0 1 3.615.302L7.74 4.377a.876.876 0 0 1 0 1.246L3.615 9.698A.87.87 0 0 1 3 9.95"
+      transform="rotate(-90, 5, 5)"
+    />
+  </svg>
+);
+
+export const ChevronDown = ({ size }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 10 10"
+    xmlns="http://www.w3.org/2000/svg"
+    className="inline-block mx-1"
+  >
+    <path
+      d="M3 9.95a.875.875 0 0 1-.615-1.498L5.88 5 2.385 1.547A.875.875 0 0 1 3.615.302L7.74 4.377a.876.876 0 0 1 0 1.246L3.615 9.698A.87.87 0 0 1 3 9.95"
+      transform="rotate(90, 5, 5)"
+    />
+  </svg>
+);
